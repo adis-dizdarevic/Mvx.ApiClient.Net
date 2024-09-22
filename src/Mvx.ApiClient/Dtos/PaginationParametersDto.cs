708b868c01937ad5ff53682c27fecd156e4313ca
Adis Dizdarevic <178735164+adis-dizdarevic@users.noreply.github.com>
2024-09-22 21:47:11 +0200

    improvements

---
+++ src/Mvx.ApiClient/Dtos/PaginationParametersDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Mvx.ApiClient.Dtos;
+
+/// <summary>
+/// Dto for handling pagination parameters
+/// </summary>
+public class PaginationParametersDto
+{
+    /// <summary>
+    /// The number of items to retrieve
+    /// </summary>
+    public int? Limit { get; set; }
+    
+    /// <summary>
+    /// The number of items to skip
+    /// </summary>
+    public int? Offset { get; set; }
+}
--- src/Mvx.ApiClient/ExtensionMethods/HttpClientExtensions.cs
+++ src/Mvx.ApiClient/ExtensionMethods/HttpClientExtensions.cs
@@ -1,12 +1,51 @@
-﻿using System.Net.Http.Json;
+﻿using Mvx.ApiClient.Dtos;
+using System.Net.Http.Json;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Mvx.ApiClient.ExtensionMethods;
 
-public class HttpClientExtensions
+internal static class HttpClientExtensions
 {
-    public static JsonSerializerOptions DefaultOptions { get; } = new JsonSerializerOptions
+    internal static async Task<T> GetWithQueryParametersAsync<T>(this HttpClient httpClient, string requestUri, QueryParametersDto? queryParameters = null)
+    {
+        requestUri = BuildRequestUri(requestUri, queryParameters);
+        var response = await httpClient.GetFromJsonAsync<T>(requestUri, DefaultJsonSerializerOptions);
+
+        return response;
+    }
+
+    internal static string BuildRequestUri(string requestUri, QueryParametersDto? queryParameters = null)
+    {
+        var queryDictionary = new Dictionary<string, string>();
+        
+        if (queryParameters?.Pagination is { Limit: not null })
+        {
+            queryDictionary.Add("size", queryParameters.Pagination.Limit.Value.ToString());
+        }
+
+        if (queryParameters?.Pagination is { Offset: not null })
+        {
+            queryDictionary.Add("from", queryParameters.Pagination.Offset.Value.ToString());
+        }
+
+        if (queryParameters?.Data?.Fields is not null && queryParameters.Data.Fields.Any())
+        {
+            queryDictionary.Add("fields", string.Join(",", queryParameters.Data.Fields));
+        }
+        
+        if (!string.IsNullOrEmpty(queryParameters?.Data?.Extract))
+        {
+            queryDictionary.Add("extract", queryParameters.Data.Extract);
+        }
+        
+        var queryString = string.Join("&", queryDictionary.Select(param => $"{param.Key}={param.Value}"));
+        requestUri = string.IsNullOrEmpty(queryString) ? requestUri : $"{requestUri}?{queryString}";
+
+        return requestUri;
+    }
+    
+    private static JsonSerializerOptions DefaultJsonSerializerOptions { get; } = new()
     {
         PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
         Converters =
@@ -14,36 +53,4 @@ public class HttpClientExtensions
             new JsonStringEnumConverter()
         }
     };
-
-    public static async Task<T> GetWithQueryParametersAsync<T>(HttpClient httpClient, string requestUri, int? limit = null, int? offset = null, IEnumerable<string>? fields = null, string? extract = null)
-    {
-        var queryParameters = new Dictionary<string, string>();
-
-        if (limit.HasValue)
-        {
-            queryParameters.Add("size", limit.Value.ToString());
-        }
-
-        if (offset.HasValue)
-        {
-            queryParameters.Add("from", offset.Value.ToString());
-        }
-
-        if (fields is not null && fields.Any())
-        {
-            queryParameters.Add("fields", string.Join(",", fields));
-        }
-        
-        if (!string.IsNullOrEmpty(extract))
-        {
-            queryParameters.Add("extract", extract);
-        }
-        
-        var queryString = string.Join("&", queryParameters.Select(param => $"{param.Key}={param.Value}"));
-        var requestUrl = string.IsNullOrEmpty(queryString) ? requestUri : $"{requestUri}?{queryString}";
-        
-        var response = await httpClient.GetFromJsonAsync<T>(requestUrl);
-
-        return response;
-    }
 }
