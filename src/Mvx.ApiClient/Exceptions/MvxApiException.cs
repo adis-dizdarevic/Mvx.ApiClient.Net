19e88cc086404768d2f2169effdf5b29a7bc36ec
Adis Dizdarevic <178735164+adis-dizdarevic@users.noreply.github.com>
2024-10-02 20:35:04 +0200

    cleanups

--- src/Mvx.ApiClient/Exceptions/MvxApiException.cs
+++ src/Mvx.ApiClient/Exceptions/MvxApiException.cs
@@ -1,5 +1,8 @@
 ﻿namespace Mvx.ApiClient.Exceptions;
 
+/// <summary>
+/// The exception that is thrown when the MultiversX API returns a status code between 400 and 499
+/// </summary>
 public class MvxApiException : Exception
 {
     public MvxApiException(string message, string error, int statusCode) : base(message)
